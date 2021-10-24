f87a627e4b1c4e55197639d4587917aab0a8c8e1
Geraldhi Mahayadnya <geraldiaditya1111@gmail.com>
2021-10-24 19:30:56 +0700

    creating database table schema

---
+++ migrations/1635038219709_table-station.js
@@ -0,0 +1,64 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+    pgm.createTable('stations', {
+        uuid: {
+            type: 'VARCHAR(64)',
+            primaryKey: true,
+        },
+        web_id: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        short_url: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        long_url: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        url_id: {
+            type: 'VARCHAR(5)',
+            notNull: true,
+        },
+    });
+    pgm.createTable('sensors', {
+        id_sensor: {
+            type: 'VARCHAR(16)',
+            primaryKey: true,
+        },
+        type: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        value: {
+            type: 'NUMERIC(8,2)',
+            notNull: true,
+        },
+        value_unit: {
+            type: 'TEXT',
+        },
+        display_unit: {
+            type: 'TEXT',
+        },
+        minvalue: {
+            type: 'TEXT',
+        },
+        maxvalue: {
+            type: 'TEXT',
+        },
+        uuid_sensor: {
+            type: 'VARCHAR(64)',
+            references: 'stations',
+            onDelete: 'CASCADE',
+        },
+    });
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable('sensors');
+    pgm.dropTable('stations');
+};
